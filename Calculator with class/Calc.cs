dc3072fdd4a7c2d1276ae55313585fec7ca4fd80
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2019-09-03 12:14:20 +1200

    Add project files.

---
+++ Calculator with class/Calc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_with_class
+{
+    public class Calc
+    {
+        /// <summary>
+        /// Declaring the properties for this clas
+        /// </summary>
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        //to show +, -, *, / symbols in you combubo box you need to declare another properties
+        public string SelectedSymbol { get; set; }
+        //Now its time to creat the methods
+
+        /// <summary>
+        /// To add two numbers
+        /// </summary>
+        public double Add(double one, double two)
+        {
+            return one + two;
+        }
+        /// <summary>
+        /// To Multiply two numbers
+        /// </summary>
+        public double Multiply(double one, double two)
+        {
+            return one * two;
+        }
+        /// <summary>
+        /// To Subtract two numbers
+        /// </summary>
+        public double Subtract(double one, double two)
+        {
+            return one - two;
+        }
+        /// <summary>
+        /// To Divided two numbers
+        /// </summary>
+        public double Divide(double one, double two)
+        {
+            return one / two;
+        }
+        /// <summary>
+        /// Method to load symbols to the combobox
+        /// </summary>
+        /// <returns></returns>
+        public string [] LoadSymbols()
+        {
+            string[] ComboBoxItems = new[] { "Select operation", "+", "-", "*", "/" };
+            return ComboBoxItems;
+        }
+        public double SelectOperation()
+        {
+            double Answer = 0;
+            switch(SelectedSymbol)
+            {
+                case "+":
+                    Answer = Add(Num1, Num2);
+                    break;
+                case "-":
+                    Answer = Subtract(Num1, Num2);
+                    break;
+                case "*":
+                    Answer = Multiply(Num1, Num2);
+                    break;
+                case "/":
+                    Answer = Divide(Num1, Num2);
+                    break;
+            }
+            return Answer;
+        }
+    }
+}
---
+++ Calculator with class/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Calculator_with_class
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.cbxOperation = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.ItemHeight = 16;
+            this.lbxOutput.Location = new System.Drawing.Point(42, 121);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(257, 212);
+            this.lbxOutput.TabIndex = 0;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblAnswer.Location = new System.Drawing.Point(42, 63);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(257, 55);
+            this.lblAnswer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(462, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(568, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number 2";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(443, 130);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 3;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(555, 130);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(438, 189);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(105, 56);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Caluclate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // cbxOperation
+            // 
+            this.cbxOperation.FormattingEnabled = true;
+            this.cbxOperation.Location = new System.Drawing.Point(606, 206);
+            this.cbxOperation.Name = "cbxOperation";
+            this.cbxOperation.Size = new System.Drawing.Size(121, 24);
+            this.cbxOperation.TabIndex = 5;
+            this.cbxOperation.SelectedIndexChanged += new System.EventHandler(this.CbxOperation_SelectedIndexChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(429, 268);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(114, 46);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(561, 268);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 46);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.cbxOperation);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lbxOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ComboBox cbxOperation;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
